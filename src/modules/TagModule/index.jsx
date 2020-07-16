d2f0a74a1778ead20bd1993e8c0b4fa878689c17
Philip <p208p2002@gmail.com>
2020-07-16 10:16:53 +0800

    no message

--- src/modules/TagModule/index.jsx
+++ src/modules/TagModule/index.jsx
@@ -3,14 +3,14 @@ import styled from 'styled-components'
 
 
 const TagBlock = styled.pre`
-    font-size:${(props)=>props.fontSize};
+    font-size:${(props) => props.fontSize};
 `
 
 export class index extends Component {
     constructor(props) {
         super(props)
         this.state = {
-            fontSize:18,
+            fontSize: 18,
             // eslint-disable-next-line
             cj_text: '福建高等法院金門分院刑事裁定　　　　　 100年度聲字第10號\n\
 聲　請　人\n\
@@ -23,30 +23,45 @@ export class index extends Component {
         }
     }
 
-    setFontSize = (newSize)=>{
+    setFontSize = (newSize) => {
         this.setState({
-            fontSize:newSize
+            fontSize: newSize
         })
     }
 
+    tagWords = (e) => {
+        let selection = window.getSelection();
+        let selectWord = selection.toString();
+        let tag_start = selection.anchorOffset;
+        let tag_end = selection.focusOffset - 1;
+        let selectTag = {
+            val:selectWord,
+            tag_start,
+            tag_end
+        }
+        console.log(selectTag)
+    }
+
     render() {
-        let { cj_text,fontSize } = this.state
+        let { cj_text, fontSize } = this.state
         return (
             <div>
                 <div>
                     <label htmlFor="">font-size:{fontSize}&nbsp;&nbsp;</label>
-                    <button onClick={()=>{this.setFontSize(fontSize+1)}}>+</button>
-                    <button onClick={()=>{this.setFontSize(fontSize-1)}}>-</button>
+                    <button onClick={() => { this.setFontSize(fontSize + 1) }}>+</button>
+                    <button onClick={() => { this.setFontSize(fontSize - 1) }}>-</button>
                 </div>
                 <hr />
-                <button>下一篇</button>
+                <button>下一篇(雙擊)</button>
                 <button>儲存</button>
                 <hr />
-                <TagBlock fontSize={`${fontSize}px`}>
+                <TagBlock
+                    fontSize={`${fontSize}px`}
+                    onMouseUp={(e) => this.tagWords(e)}
+                >
                     {cj_text}
                 </TagBlock>
                 <hr />
-                <button>開始標註</button>
             </div>
         )
     }
