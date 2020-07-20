ad603d496fe354ba550fad04eed5c28750470007
Philip <p208p2002@gmail.com>
2020-07-20 09:55:34 +0800

    no message

--- src/modules/TagModule/action.js
+++ src/modules/TagModule/action.js
@@ -1,5 +1,5 @@
-// import axios
 import { initApp } from '../action'
+const axios = require('axios');
 
 export const submitTag = (tagWordObject)=>{
     return {
@@ -9,12 +9,16 @@ export const submitTag = (tagWordObject)=>{
 }
 
 export const getUnlabelDoc = ()=>{
-    let unlabelDoc = '福建高等法院金門分院刑事裁定　　　　　 100年度聲字第10號\n聲　請　人\n即　被　告　許丕燕\n上列聲請人即被告因違反毒品危害防制條例罪案件（本院99年度\n上重訴字第2 號違反毒品危害防制條例罪案件），聲請具保停止\n羈押，本院裁定如下：\n主  文\n聲請駁回。'
+    // let unlabelDoc = '福建高等法院金門分院刑事裁定　　　　　 100年度聲字第10號\n聲　請　人\n即　被　告　許丕燕\n上列聲請人即被告因違反毒品危害防制條例罪案件（本院99年度\n上重訴字第2 號違反毒品危害防制條例罪案件），聲請具保停止\n羈押，本院裁定如下：\n主  文\n聲請駁回。'
     return (dispatch)=>{
         dispatch(initApp())
         dispatch({type:"TAG_GET_UNLABEL_DOC_START"})
-        setTimeout(()=>{
-            dispatch({type:"TAG_GET_UNLABEL_DOC_SUCCESS",unlabelDoc})
-        },1000)
+        axios.get("http://140.120.13.242:15004/unlabel_doc")
+            .then((res)=>{
+                let { verdict } = res.data
+                verdict = JSON.parse(verdict)
+                let { content } = verdict
+                dispatch({type:"TAG_GET_UNLABEL_DOC_SUCCESS",unlabelDoc:content})
+            })
     }
 }
